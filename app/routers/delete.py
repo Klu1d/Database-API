1d0db21ea56bf0f685050669dbdc5c9dc678765e
Klu1d <105982046+Klu1d@users.noreply.github.com>
2025-04-25 16:31:26 +0300

    Update delete.py

--- app/routers/delete.py
+++ app/routers/delete.py
@@ -9,24 +9,24 @@ router = APIRouter()
 
 
 @router.delete("/delete/company/{id}")
-async def update_company(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
+async def delete_company(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
     message = await crud.delete(session, schemas.Companies, id)
     return message
 
 
 @router.delete("/delete/employee/{id}")
-async def update_employee(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
+async def delete_employee(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
     message = await crud.delete(session, schemas.Employees, id)
     return message
 
 
 @router.delete("/delete/camera/{id}")
-async def update_cameras(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
+async def delete_cameras(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
     message = await crud.delete(session, schemas.Cameras, id)
     return message
 
 
 @router.delete("/delete/event/{id}")
-async def update_event(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
+async def delete_event(id: int, session: AsyncSession = Depends(database.sessions), api_key: str = Depends(secret)):
     message = await crud.delete(session, schemas.Events, id)
     return message
