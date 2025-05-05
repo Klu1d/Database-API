3e1115f41defa23afe58d40618e99d68893ecf6c
Klu1d <asabaev.islam@icloud.com>
2025-05-05 19:18:10 +0300

    refactor: update Docker config and app entry point

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from database import Base, engine
 from fastapi import FastAPI
-from routers import create, delete, read, update
+from routers import create, read, update, delete, media
 
 
 @asynccontextmanager
@@ -23,6 +23,7 @@ app.include_router(create.router)
 app.include_router(read.router)
 app.include_router(update.router)
 app.include_router(delete.router)
+app.include_router(media.router, tags=['media'], prefix='media')
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8084)
+    uvicorn.run(app)
